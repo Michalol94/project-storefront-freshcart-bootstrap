7ad6385696d663e3cf3fcbf0b1617ccb9f5a2b9f
Michalol94 <michal.olech94@gmail.com>
2023-01-21 23:13:08 +0100

    sort products

--- src/app/components/category-products/category-products.component-module.ts
+++ src/app/components/category-products/category-products.component-module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CategoryProductsComponent } from './category-products.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   declarations: [CategoryProductsComponent],
   providers: [],
   exports: [CategoryProductsComponent],
--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -4,9 +4,13 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, of } from 'rxjs';
+import { filter, map, switchMap, startWith } from 'rxjs/operators';
 import { CategoryModel } from '../../models/category.model';
+import { ProductModel } from '../../models/product.model';
 import { CategoryService } from '../../services/category.service';
+import { ProductService } from '../../services/product.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-category-products',
@@ -16,6 +20,8 @@ import { CategoryService } from '../../services/category.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductsComponent {
+  readonly sort: FormControl = new FormControl('Featured');
+
   readonly categories$: Observable<CategoryModel[]> =
     this._categoryService.getAllCategories();
 
@@ -27,8 +33,55 @@ export class CategoryProductsComponent {
     switchMap((id) => this._categoryService.getOnecategory(id))
   );
 
+  readonly sort$: Observable<string> = this.sort.valueChanges.pipe(
+    startWith(this.sort.value)
+  );
+
+  readonly categoryProducts$: Observable<ProductModel[]> = combineLatest([
+    this.categoryId$,
+    this._productService.getAll(),
+    this.sort$,
+  ]).pipe(
+    map(([catId, products, sort]) => {
+      const prods = products.filter((product) => product.categoryId === catId);
+      if (sort === 'Featured')
+        return prods.sort((a, b) => {
+          if (a.featureValue > b.featureValue) return -1;
+          if (a.featureValue < b.featureValue) return 1;
+          return 0;
+        });
+      if (sort === 'Price: Low to High')
+        return prods.sort((a, b) => {
+          if (a.price > b.price) return 1;
+          if (a.price < b.price) return -1;
+          return 0;
+        });
+      if (sort === 'Price: High to Low')
+        return prods.sort((a, b) => {
+          if (a.price > b.price) return -1;
+          if (a.price < b.price) return 1;
+          return 0;
+        });
+      if (sort === 'Avg. Rating')
+        return prods.sort((a, b) => {
+          if (a.ratingValue > b.ratingValue) return 1;
+          if (a.ratingValue < b.ratingValue) return -1;
+          return 0;
+        });
+      return prods;
+    })
+  );
+
+  selectOptions$: Observable<string[]> = of([
+    'Featured',
+    'Price: Low to High',
+    'Price: High to Low',
+    'Avg. Rating',
+  ]);
+
   constructor(
     private _categoryService: CategoryService,
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute,
+    private _productService: ProductService
   ) {}
 }
