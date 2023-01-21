da99a06e933bc7d32e41041d87da49fbcc7f3498
Michalol94 <michal.olech94@gmail.com>
2023-01-21 11:51:35 +0100

    feat/store product component

--- src/app/components/store-products/store-products.component-module.ts
+++ src/app/components/store-products/store-products.component-module.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StoreProductsComponent } from './store-products.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [StoreProductsComponent],
   providers: [],
-  exports: [StoreProductsComponent]
+  exports: [StoreProductsComponent],
 })
-export class StoreProductsComponentModule {
-}
+export class StoreProductsComponentModule {}
