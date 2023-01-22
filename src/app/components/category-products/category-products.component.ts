ba14a403a5d2ecbf4c1e82ed523613f25bf6364d
Michalol94 <michal.olech94@gmail.com>
2023-01-22 12:45:21 +0100

    pagination

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -3,14 +3,19 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { BehaviorSubject, Observable, combineLatest, of } from 'rxjs';
-import { filter, map, switchMap, startWith } from 'rxjs/operators';
+import { filter, map, shareReplay, startWith, switchMap } from 'rxjs/operators';
 import { CategoryModel } from '../../models/category.model';
 import { ProductModel } from '../../models/product.model';
 import { CategoryService } from '../../services/category.service';
 import { ProductService } from '../../services/product.service';
-import { FormControl } from '@angular/forms';
+
+interface Pagination {
+  pageSize: number;
+  pageNumber: number;
+}
 
 @Component({
   selector: 'app-category-products',
@@ -20,6 +25,14 @@ import { FormControl } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductsComponent {
+  private _paginationSubject: BehaviorSubject<Pagination> =
+    new BehaviorSubject<Pagination>({ pageSize: 5, pageNumber: 1 });
+
+  public pagination$: Observable<Pagination> =
+    this._paginationSubject.asObservable();
+
+  readonly limitProducts$: Observable<number[]> = of([5, 10, 15]);
+
   readonly sort: FormControl = new FormControl('Featured');
 
   readonly categories$: Observable<CategoryModel[]> =
@@ -69,6 +82,41 @@ export class CategoryProductsComponent {
           return 0;
         });
       return prods;
+    }),
+    shareReplay(1)
+  );
+
+  readonly slicedProducts$: Observable<ProductModel[]> = combineLatest([
+    this.categoryProducts$,
+    this.pagination$,
+  ]).pipe(
+    map(([products, pagination]) =>
+      products.slice(
+        (pagination.pageNumber - 1) * pagination.pageSize,
+        pagination.pageNumber * pagination.pageSize
+      )
+    ),
+    shareReplay(1)
+  );
+
+  readonly updatedPages$: Observable<number[]> = combineLatest([
+    this.categoryProducts$,
+    this.pagination$,
+  ]).pipe(
+    map(([products, pagination]) => {
+      const max: number = Math.ceil(products.length / pagination.pageSize);
+      const updatedPages: number[] = [1];
+      for (
+        let i = Math.max(2, pagination.pageNumber - 1);
+        i <= Math.min(max - 1, pagination.pageNumber + 1);
+        i++
+      ) {
+        updatedPages.push(i);
+      }
+      if (max > 1) {
+        updatedPages.push(max);
+      }
+      return updatedPages;
     })
   );
 
@@ -84,4 +132,18 @@ export class CategoryProductsComponent {
     private _activatedRoute: ActivatedRoute,
     private _productService: ProductService
   ) {}
+
+  changePageLimit(pageSize: number): void {
+    this._paginationSubject.next({
+      ...this._paginationSubject.value,
+      pageSize,
+      pageNumber: 1,
+    });
+  }
+  changePageNumber(pageNumber: number): void {
+    this._paginationSubject.next({
+      ...this._paginationSubject.value,
+      pageNumber,
+    });
+  }
 }
